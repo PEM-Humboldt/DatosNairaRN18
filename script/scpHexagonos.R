069b52e5a3f94c7b9fdbb2e38d63e0c7c493b75b
MaIsAp <zeamaiz@gmail.com>
2020-02-05 09:38:48 -0500

    Se crearon los ID por especie y los rangos de fechas para crear las matrices de ocupacion. Pendiente organizar fechas y datos malos

--- script/scpHexagonos.R
+++ script/scpHexagonos.R
@@ -3,9 +3,10 @@
 # Toda variable que comience con v es vector, d dataframe, l lista
 
 library(readxl)
-library(purrr)
-library(tidyr)
-library(dplyr)
+# library(purrr)
+# library(tidyr)
+library(lubridate)
+library(tidyverse)
 
 #obtengo ruta
 vHexFile <- list.files("./data",full.names = T)
@@ -39,6 +40,85 @@ funRenameICount <- function(y){
 
 lHexData2 <- map(lHexData, function(x) map(x, funRenameICount))
 lHexData3 <- map(lHexData2, bind_rows, .id="ID_1")
-lHexData4 <- lHexData3 %>% bind_rows(.id="Hexagono")
+dHexData <- lHexData3 %>% bind_rows(.id="Hexagono")
 
-write.csv(lHexData4,"./Salidas/data_hexagonos.csv")
+write.csv(dHexData,"./Salidas/data_hexagonos.csv")
+
+#Crear variable de fecha en formato
+dHexData$eventDate <- ymd(dHexData$eventDate)
+dHexData$eventTimeDate <- ymd_hms(paste(dHexData$eventDate, substr(dHexData$eventTime,12,19)))
+# format(as.POSIXct(Time,format="%H:%M:%S"),"%H")
+
+#Crear ID especies
+dHexData$Species <- gsub("\  ","\ ",dHexData$Species)
+dSpecies <- dHexData %>% group_by(Species) %>% summarise(n=n())
+
+dHexData$eventID <- gsub("Station: ","",dHexData$eventID) # dejo solo codigo de estacion
+
+dHexDataGen <- dHexData %>% separate(Species, c("Genero","Epiteto","Subesp"),sep="\ ")
+dGeneros <- dHexDataGen %>% group_by(Genero) %>% summarise(n=n()) %>% rowid_to_column("ID_sp") # creo el ID por genero
+
+dHexDataGen <- left_join(dHexDataGen,dGeneros %>% select(-n),by="Genero") # asigno ID por genero a datos
+
+#Crear grupos de fechas segun cantidad de dias
+dCamaraFecha <- dHexDataGen %>% group_by(eventID,eventDate) %>% summarise(n=n()) # encuentro grupos de fecha por estacion
+temp <- dCamaraFecha %>% group_by(eventID) %>% summarise(MinFecha=min(eventDate),MaxFecha=max(eventDate)) %>% na.omit()
+
+vCantDias <- 7 # cantidad de dias en los que se van a agrupar las observaciones
+lGruposDias <- list()
+for(i in 1:nrow(temp)){
+  # lGruposDias[[i]] <- c(seq(temp$MinFecha[i],temp$MaxFecha[i],vCantDias),temp$MaxFecha[i]) # usar si toca incluir la ultima fecha aunque no cumpla numero de dias
+  lGruposDias[[i]] <- seq(temp$MinFecha[i],temp$MaxFecha[i],vCantDias)
+}
+
+#crear dataframe con columnas de rangos minimos y maximos y ID por grupo de fecha
+#aprovechar este paso para chequear fechas raras
+names(lGruposDias) <- temp$eventID
+lGruposDias <- map(lGruposDias, function(x) data_frame(RangoMin=x))
+lGruposDias <- map(lGruposDias, function(x) {
+  x$RangoMax <- c(x$RangoMin[-1],NA)
+  x <- x %>% rowid_to_column("ID_Rango")
+  return(x[-nrow(x),])
+})
+
+dGruposDias <- lGruposDias %>% bind_rows(.id="eventID")
+
+# asignar grupo de fecha a cada observacion   
+dHexDataGen$ID_Rango <- NA
+for(i in 1:nrow(dHexDataGen)){
+  cameraID <- dHexDataGen$eventID[i]
+  rangoMin <- dGruposDias$RangoMin[dGruposDias$eventID%in%cameraID]
+  rangoMax <- dGruposDias$RangoMax[dGruposDias$eventID%in%cameraID]
+  
+  fecha <- dHexDataGen$eventDate[i]
+  condi <- fecha >= rangoMin & fecha <= rangoMax
+  if(sum(condi)==1){
+    dHexDataGen$ID_Rango[i] <- dGruposDias$ID_Rango[dGruposDias$eventID%in%cameraID][condi]
+  }
+}
+
+saveRDS(dHexDataGen,"./DatosFinales.R")
+
+# ------------------------------------------
+# Matriz de ocupacion por camara
+# ------------------------------------------
+test <- dHexDataGen %>% group_by(eventID,ID_sp,Genero, ID_Rango) %>% summarise(n=n())
+test2 <- test %>% spread(ID_Rango, n)
+
+dHexCam <- dHexDataGen %>% group_by(Hexagono,eventID,ID_Rango,Genero) %>% summarise(Cant=sum(as.numeric(Numero)))
+
+ggplot(dHexCam %>% filter(Hexagono=="Hexagono23"), aes(x=ID_Rango,y=Cant))+geom_point()+facet_grid(eventID~Genero)
+
+# ------------------------------------------
+# Matriz de ocupacion por hexagono
+# ------------------------------------------
+dHexSp <- dHexDataGen %>% group_by(Hexagono,ID_Rango,Genero) %>% summarise(Cant=sum(as.numeric(Numero)))
+
+ggplot(dHexSp %>% filter(Hexagono=="Hexagono23"), aes(x=ID_Rango,y=Cant))+geom_point()+facet_wrap(Genero~.,ncol=9)
+
+
+
+# ------------------------------------------
+# Salida DarwinCore
+# ------------------------------------------
+
